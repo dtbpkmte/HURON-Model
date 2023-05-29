08988d71a4afae3ba1002fc8cf23859bf68bf1ba
Duc Doan <dtbpkmte@gmail.com>
2023-05-29 14:03:14 -0400

    test

--- huron_description/test.m
+++ huron_description/test.m
@@ -3,4 +3,4 @@ clc; clear; close all
 robot = importrobot("urdf/huron.urdf", "DataFormat", "row");
 % robot.Gravity = [0 0 -9.8];
 % show(robot, "Visuals","off","Collisions","on")
-show(robot, "Visuals", "on")
+show(robot, "Visuals", "off")
