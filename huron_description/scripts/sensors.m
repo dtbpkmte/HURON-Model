becf0f5e6526af9f5cf704bd3433d4e7f07adae1
Duc Doan <dtbpkmte@gmail.com>
2023-06-20 17:12:49 -0400

    WIP

--- huron_description/scripts/sensors.m
+++ huron_description/scripts/sensors.m
@@ -9,8 +9,8 @@ JointStates = rossubscriber('/huron/joint_states');
 
 Imu=rossubscriber('/huron/sensor/imu');
 
-r1_force=rossubscriber('/huron/sensor/r1_ft_sensor');
-l1_force=rossubscriber('/huron/sensor/l1_ft_sensor');
+r1_ft_sensor = rossubscriber('/huron/sensor/r1_ft_sensor');
+l1_ft_sensor = rossubscriber('/huron/sensor/l1_ft_sensor');
 % contact_sensor=rossubscriber('/fixed_jaw_contact_sensor_state');
 
 % Vertical distance from the load cell to bottom of the foot
@@ -19,6 +19,9 @@ d = 0.0983224252792114;
 p1 = [0; 0.0775; d];
 % Position of right sensor
 p2 = [0; -0.0775; d];
+
+% x_ZMP limits
+x_ZMP_lims = [-50e-3, 157.88e-3]; % m
 
 tau = rosmessage(joint_effort_pub);
 tau.Data = [0, 0, 0, 0, 0, 0,
@@ -43,39 +46,43 @@ tau_L = zeros(prealloc_size, 3); % [tau_ix tau_iy tau_iz]
 f_L = zeros(prealloc_size, 3); % [f_ix f_iy f_iz]
 tau_R = zeros(prealloc_size, 3); % [tau_ix tau_iy tau_iz]
 f_R = zeros(prealloc_size, 3); % [f_ix f_iy f_iz]
+
+p_ZMP_x = zeros(prealloc_size, 1);
  
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-while t < 25 && i <= prealloc_size
+while t < 15 && i <= prealloc_size
     t = rostime('now');
     t = t.Sec + t.Nsec/1e9;
     time(i) = t;
 
     %read the force sensor readings right foot
-    r1_force_sensor=receive(r1_force);
+    r1_ft = receive(r1_ft_sensor);
 
     %read the force sensor readings left foot
-    l1_force_sensor=receive(l1_force);
+    l1_ft = receive(l1_ft_sensor);
+    % 
+    % tau_R(i, :) = [
+    %     r1_force_sensor.Wrench.Torque.X, ...
+    %     r1_force_sensor.Wrench.Torque.Y, ...
+    %     r1_force_sensor.Wrench.Torque.Z
+    % ];
+    % tau_L(i, :) = [
+    %     l1_force_sensor.Wrench.Torque.X, ...
+    %     l1_force_sensor.Wrench.Torque.Y, ...
+    %     l1_force_sensor.Wrench.Torque.Z
+    % ];
+    % f_R(i, :) = [
+    %     r1_force_sensor.Wrench.Force.X, ...
+    %     r1_force_sensor.Wrench.Force.Y, ...
+    %     r1_force_sensor.Wrench.Force.Z
+    % ];
+    % f_L(i, :) = [
+    %     l1_force_sensor.Wrench.Force.X, ...
+    %     l1_force_sensor.Wrench.Force.Y, ...
+    %     l1_force_sensor.Wrench.Force.Z
+    % ];
 
-    tau_R(i, :) = [
-        r1_force_sensor.Wrench.Torque.X, ...
-        r1_force_sensor.Wrench.Torque.Y, ...
-        r1_force_sensor.Wrench.Torque.Z
-    ];
-    tau_L(i, :) = [
-        l1_force_sensor.Wrench.Torque.X, ...
-        l1_force_sensor.Wrench.Torque.Y, ...
-        l1_force_sensor.Wrench.Torque.Z
-    ];
-    f_R(i, :) = [
-        r1_force_sensor.Wrench.Force.X, ...
-        r1_force_sensor.Wrench.Force.Y, ...
-        r1_force_sensor.Wrench.Force.Z
-    ];
-    f_L(i, :) = [
-        l1_force_sensor.Wrench.Force.X, ...
-        l1_force_sensor.Wrench.Force.Y, ...
-        l1_force_sensor.Wrench.Force.Z
-    ];
+    p_ZMP_x(i) = readCOP(r1_ft, l1_ft);
 
 
     send(joint_effort_pub, tau);
@@ -88,54 +95,73 @@ end
 
 % Trim out unfilled data
 time = time(1:i-1);
-tau_L = tau_L(1:i-1, :);
-tau_R = tau_R(1:i-1, :);
-f_L = f_L(1:i-1, :);
-f_R = f_R(1:i-1, :);
+% tau_L = tau_L(1:i-1, :);
+% tau_R = tau_R(1:i-1, :);
+% f_L = f_L(1:i-1, :);
+% f_R = f_R(1:i-1, :);
+p_ZMP_x = p_ZMP_x(1:i-1, :);
 
 % Add the equations to calculate the COP 
-figure;
-subplot(1, 2, 1)
-plot(time, tau_L)
-title('Left foot Torque')
-subplot(1, 2, 2)
-plot(time, f_L)
-title('Left foot Force')
-
-figure;
-subplot(1, 2, 1)
-plot(time, tau_R)
-title('Right foot Torque')
-subplot(1, 2, 2)
-plot(time, f_R)
-title('Right foot Force')
+% figure;
+% subplot(1, 2, 1)
+% plot(time, tau_L)
+% title('Left foot Torque')
+% subplot(1, 2, 2)
+% plot(time, f_L)
+% title('Left foot Force')
+% 
+% figure;
+% subplot(1, 2, 1)
+% plot(time, tau_R)
+% title('Right foot Torque')
+% subplot(1, 2, 2)
+% plot(time, f_R)
+% title('Right foot Force')
 
 % Check correctness of force sensors by mass
-robot_mass = 37.2063;
-g = 9.81;
-figure;
-plot(time, robot_mass * g * ones(size(time))); % correct value
-hold on
-f_total = f_L(:, 1) + f_R(:, 1);
-plot(time, f_total);
-hold off
+% robot_mass = 37.2063;
+% g = 9.81;
+% figure;
+% plot(time, robot_mass * g * ones(size(time))); % correct value
+% hold on
+% f_total = f_L(:, 1) + f_R(:, 1);
+% plot(time, f_total);
+% hold off
 
 % ZMP plot
 % p_Ly = (f_Lz * l_force_sensor_pos(:, 2)) ./ sum(f_Lz, 2);
 % p_Ry = (f_Rz * r_force_sensor_pos(:, 2)) ./ sum(f_Rz, 2);
-p_ZMP_x = (tau_L(:, 2) + d*f_L(:, 3) + tau_R(:, 2) + d*f_R(:, 3)) ./ (f_L(:, 1) + f_R(:, 1));
+% p_ZMP_x = (tau_L(:, 2) + d*f_L(:, 3) + tau_R(:, 2) + d*f_R(:, 3)) ./ (f_L(:, 1) + f_R(:, 1));
 figure;
 plot(time, p_ZMP_x);
+title('x_ZMP raw')
+xlabel('time (s)'); ylabel('x (m)');
+hold on
+plot(time, x_ZMP_lims(1) * ones(size(time)))
+plot(time, x_ZMP_lims(2) * ones(size(time)))
+hold off
 
 % Filtered ZMP plot (LPF)
-p_ZMP_x_filtered = lowpass(p_ZMP_x, 0.05);
+p_ZMP_x_filtered = lowpass(p_ZMP_x, 10, 100);
 figure;
 plot(time, p_ZMP_x_filtered);
+title('x_ZMP LPF')
+xlabel('time (s)'); ylabel('x (m)');
+hold on
+plot(time, x_ZMP_lims(1) * ones(size(time)))
+plot(time, x_ZMP_lims(2) * ones(size(time)))
+hold off
 
-% Median filter, 10th order
-p_ZMP_med_filt = medfilt1(p_ZMP_x, 10);
+% Median filter, 45th order
+p_ZMP_med_filt = medfilt1(p_ZMP_x, 45);
 figure;
 plot(time, p_ZMP_med_filt);
+title('x_ZMP med')
+xlabel('time (s)'); ylabel('x (m)');
+hold on
+plot(time, x_ZMP_lims(1) * ones(size(time)))
+plot(time, x_ZMP_lims(2) * ones(size(time)))
+hold off
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 send(joint_effort_pub, tau);
--- huron_description/test.m
+++ huron_description/test.m
@@ -4,3 +4,4 @@ robot = importrobot("urdf/huron.urdf", "DataFormat", "row");
 % robot.Gravity = [0 0 -9.8];
 % show(robot, "Visuals","off","Collisions","on")
 show(robot, "Visuals", "on")
+centerOfMass(robot)
